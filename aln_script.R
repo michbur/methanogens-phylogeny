4b439068030826d5585c0aa63fdf7b512c2517cb
michbur <michalburdukiewicz@gmail.com>
2017-06-29 14:07:11 +0200

    finished

--- aln_script.R
+++ aln_script.R
@@ -1,7 +1,16 @@
 library(msa)
+library(seqinr)
+library(ape)
+library(phangorn)
+library(phytools)
+library(ggtree)
 
+source("phyl_params.R")
+source("./functions/phyl.R")
 source("aln_params.R")
 source("./functions/aln.R")
+
+tmp_dir <- tempdir()
 
 seq_type <- "aa"
 
@@ -9,7 +18,9 @@ raw_seqs <- process_aln_files(read_fun("mrca_prot.fasta"))
 
 aln <- msa(raw_seqs, chosen_aln_method)
 
-msaPrettyPrint(aln, output="pdf", 
+msaPrettyPrint(aln, 
+               output="pdf",
+               file = paste0(tmp_dir, "/aln.pdf"),
                showNames="left", 
                showLogo="top", 
                shadingModeArg="chemical", 
@@ -17,3 +28,17 @@ msaPrettyPrint(aln, output="pdf",
                logoColors="chemical", 
                shadingMode="functional")
 
+conv_aln <- msaConvert(aln, type=c("seqinr::alignment"))
+
+write.fasta(sequences = conv_aln[["seq"]], names=conv_aln[["seq"]], 
+            file.out = paste0(tmp_dir, "/aln.fasta"))
+
+chosen_file <- paste0(tmp_dir, "/aln.fasta")
+
+seq_aln <- try(seq_fun[["read"]](chosen_file), silent = TRUE)
+res_tree_cmp <- seq_fun[["phyl"]](seq_aln)
+plot_tree(res_tree_cmp)
+
+svg(paste0(tmp_dir, "/tmp_name.svg"), width = 7, height = 0.45*sum(!is.na(fort_tree[["label"]])), pointsize = 12)
+plot_tree(res_tree_cmp)
+dev.off()
--- phylogeny_script.R
+++ phylogeny_script.R
@@ -1,11 +1,4 @@
-library(msa)
-library(ape)
-library(phangorn)
-library(phytools)
-library(ggtree)
 
-source("phyl_params.R")
-source("./functions/phyl.R")
 
 seq_type <- "aa"
 
@@ -19,24 +12,14 @@ full_aln <- msa(sek, chosen_aln_method)
 
 conv_aln <- msaConvert(full_aln, type=c("seqinr::alignment"))
 
-dir_name <- tempdir()
 
 write.fasta(sequences=conv_aln[["seq"]], names=seq_aln$nam[i], file.out="seq.aln", open= "a")
 conv_aln()
 
-chosen_file <- "homo.fasta.aln"
+
 chosen_file <- "glob.fasta.aln"
 
 # zmienna wewnętrzna, wartości - dna dla DNA i aa dla aminokwasów
 seq_type <- "dna"
 
 
-
-
-seq_aln <- try(seq_fun[["read"]](chosen_file), silent = TRUE)
-res_tree_cmp <- seq_fun[["phyl"]](seq_aln)
-plot_tree(res_tree_cmp)
-
-svg("tmp_name.svg", width = 7, height = 0.45*sum(!is.na(fort_tree[["label"]])), pointsize = 12)
-plot_tree(res_tree_cmp)
-dev.off()
